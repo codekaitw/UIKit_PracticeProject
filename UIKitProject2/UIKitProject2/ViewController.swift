83c560876e0ea7711daa30f7b5c888609e928add
codekaitw <65647662+codekaitw@users.noreply.github.com>
2023-10-24 18:15:09 -0400

    add UIKit mini project 2: guess a flag

--- UIKitProject2/UIKitProject2/ViewController.swift
+++ UIKitProject2/UIKitProject2/ViewController.swift
@@ -8,12 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    var buttons: [UIButton] = []
+    var askedQuestionCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "ploand", "russia", "spain", "uk", "us"]
+        
+        buttons += [button1, button2, button3]
+        for button in buttons {
+            button.layer.borderWidth = 2
+            button.layer.borderColor = UIColor.lightGray.cgColor
+        }
+        button2.layer.borderColor = UIColor(red: 1.0, green: 0.3, blue: 0.8, alpha: 0.5).cgColor
+        
+        askQuestion()
     }
-
-
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased() + " score: \(score)"
+        
+        askedQuestionCount += 1
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong! That's flag of \(countries[sender.tag])"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        if askedQuestionCount >= 9 {
+            ac.message = "Your final score is \(score)"
+            score = 0
+            askedQuestionCount = 0
+        }
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+    }
+    
 }
 
